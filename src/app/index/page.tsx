3d7a7778729a439ae59e3fcab2d559d34ac77828
joonoo <hanwu7021@gmail.com>
2024-09-19 12:35:04 +0900

    feat.index페이지 추가

--- src/app/index/page.tsx
+++ src/app/index/page.tsx
@@ -1,9 +1,10 @@
-import { Stack, Typography } from '@mui/material';
+import { Button, Stack, Typography } from '@mui/material';
 
 export default function IndexPage() {
   return (
-    <Stack>
-      <Typography>Index Page</Typography>
+    <Stack alignItems="start" p={4}>
+      <Typography variant='h4'>서울 디지텍 고등학교 </Typography>
+      <Button variant='contained'>버튼 입니다.</Button>
     </Stack>
   );
 }
